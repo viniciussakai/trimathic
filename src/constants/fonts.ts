9310bab941045f58a9b7866012f334e8bd509f6a
Vinicius Sakai <vinicius.sakai@tuta.io>
2023-07-01 15:01:59 -0300

    fix: edit font constant

--- src/constants/fonts.ts
+++ src/constants/fonts.ts
@@ -7,9 +7,9 @@ import {
 } from "@expo-google-fonts/poppins";
 
 export const fonts = {
-  thin: Poppins_100Thin,
-  regular: Poppins_400Regular,
-  medium: Poppins_500Medium,
-  bold: Poppins_700Bold,
-  black: Poppins_900Black,
+  Poppins_100Thin,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+  Poppins_900Black,
 };
