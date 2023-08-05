acde0eb07fe5751058c9ffda2fae2070d03dafb9
Vinicius Sakai <vinicius.sakai@tuta.io>
2023-08-04 23:31:50 -0300

    feat: create search base screen

---
+++ src/app/(tabs)/search.tsx
@@ -0,0 +1,10 @@
+import { Box, Container, Text } from "@/components/UI";
+import { Input } from "@/components/UI/Input";
+
+export default function TabSearch() {
+  return (
+    <Container>
+      <Input icon="search" />
+    </Container>
+  );
+}
